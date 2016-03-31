e07869dc0a2504eeea7346520b09d53d622cf157
JaninaWinkler <nina.winkler@hotmail.com>
2016-03-31 23:59:14 +0000

    all pages styled

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,16 @@
 class MoviesController < ApplicationController
 
   def index
-    if params[:query].present?
+    if params[:query].present? && params[:runtime_in_minutes].present?
+      @movies_with_runtime = Movie.movies_by_runtime(params[:runtime_in_minutes])
+      @movies = @movies_with_runtime.where("title LIKE ? OR director LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+      puts "MOVIE"
+    elsif params[:query].present?
       @movies = Movie.where("title LIKE ? OR director LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+      puts ":query"
     elsif params[:runtime_in_minutes].present?
       @movies = Movie.movies_by_runtime(params[:runtime_in_minutes])
+    puts "runtime"
     else 
       @movies = Movie.all
     end
